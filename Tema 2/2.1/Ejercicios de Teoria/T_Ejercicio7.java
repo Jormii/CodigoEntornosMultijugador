996c15c8955259b5212a0ec8a3f9095cc78e9b91
Jorge López Natal <lopeznatalj@gmail.com>
2019-01-29 18:48:27 +0100

    Add files via upload

--- Tema 2/2.1/Ejercicios de Teoria/T_Ejercicio7.java
+++ Tema 2/2.1/Ejercicios de Teoria/T_Ejercicio7.java
@@ -1,36 +1,40 @@
 package tema2_1;
 
 import static es.urjc.etsii.code.concurrency.SimpleConcurrent.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class T_Ejercicio7 {
 
-	static final int TOTAL_VISITANTES = 2;
+	static final int TOTAL_VISITANTES = 3;
 	static volatile HashMap<String, Boolean> personasConRegalo;
-	static volatile int visitantesEnElMuseo;
+	static volatile ArrayList<String> visitantesEnElMuseo;
 
 	public static void persona(String nombre) {
 		while (true) {
 			enterMutex();
-			if (visitantesEnElMuseo == 0 && !personasConRegalo.get(nombre)) {
+			if (visitantesEnElMuseo.size() == 0 && !personasConRegalo.get(nombre)) {
 				System.out.format("[%s recibe un regalo]\n", nombre);
 				personasConRegalo.put(nombre, true);
 			}
-			System.out.format("%s: ¡Hola a las %d personas!\n", nombre, visitantesEnElMuseo++);
+			System.out.format("%s: ¡Hola a las %d personas!\n", nombre, visitantesEnElMuseo.size());
+			visitantesEnElMuseo.add(nombre);
 			exitMutex();
 
-			for (Map.Entry<String, Boolean> entry : personasConRegalo.entrySet()) {
-				if (!entry.getKey().equals(nombre)) {
-					String str = (entry.getValue()) ? "Tengo" : "No tengo";
-					System.out.format("%s: %s regalo\n", entry.getKey(), str);
+			for (String p : visitantesEnElMuseo) {
+				if (!p.equals(nombre)) {
+					boolean tieneRegalo = personasConRegalo.get(p);
+					String str = (tieneRegalo) ? "Tengo" : "No tengo";
+					System.out.format("%s: %s regalo\n", p, str);
 				}
 			}
 
 			sleepRandom(100);
 
 			enterMutex();
-			System.out.format("%s: ¡Adiós a las %d personas!\n", nombre, --visitantesEnElMuseo);
+			System.out.format("%s: ¡Adiós a las %d personas!\n", nombre, visitantesEnElMuseo.size());
+			visitantesEnElMuseo.remove(nombre);
 			exitMutex();
 
 			sleepRandom(100);
@@ -38,6 +42,7 @@ public class T_Ejercicio7 {
 	}
 
 	public static void main(String[] args) {
+		visitantesEnElMuseo = new ArrayList<String>(TOTAL_VISITANTES);
 		personasConRegalo = new HashMap<>(TOTAL_VISITANTES);
 		for (int i = 0; i < TOTAL_VISITANTES; i++) {
 			String nombre = Character.toString((char) ('A' + i));
