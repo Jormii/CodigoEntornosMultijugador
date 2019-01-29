0bb5add5423742af5e935ecb29e8ae7c5c6babc1
Jorge López Natal <lopeznatalj@gmail.com>
2019-01-29 14:11:19 +0100

    Add files via upload

---
+++ Tema 2/2.1/Ejercicios de Teoria/T_Ejercicio4b.java
@@ -0,0 +1,55 @@
+package tema2_1;
+
+import static es.urjc.etsii.code.concurrency.SimpleConcurrent.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class T_Ejercicio4b {
+
+	static final int NUMERO_CLIENTES = 5;
+	static volatile Queue<Integer>[] colaEnvios;
+	static volatile Queue<Integer>[] colaDevoluciones;
+
+	public static void cliente(int id) {
+		while (true) {
+			int unidades = new Random().nextInt(10);
+			int decenas = 10 * id;
+			int valor = decenas + unidades;
+			colaEnvios[id].add(valor);
+			System.out.format("El cliente %d ha creado el valor %d\n", id, valor);
+
+			while (colaDevoluciones[id].peek() != null) {
+				System.out.format("Valor devuelto por el servidor al cliente %d: %d\n", id, colaDevoluciones[id].remove());
+			}
+		}
+	}
+
+	public static void servidor() {
+		while (true) {
+			for (int i = 0; i < NUMERO_CLIENTES; i++) {
+				while (colaEnvios[i].peek() != null) {
+					int valorDevuelto = colaEnvios[i].remove() + 1;
+					System.out.format("El servidor devuelve al cliente %d el valor %d\n", i, valorDevuelto);
+					colaDevoluciones[i].add(valorDevuelto);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		colaEnvios = new Queue[NUMERO_CLIENTES];
+		colaDevoluciones = new Queue[NUMERO_CLIENTES];
+				
+		for (int i = 0; i < NUMERO_CLIENTES; i++) {
+			createThread("cliente", i);
+			colaEnvios[i] = new LinkedList<Integer>();
+			colaDevoluciones[i] = new LinkedList<Integer>();
+		}
+		createThread("servidor");
+
+		startThreadsAndWait();
+	}
+
+}
