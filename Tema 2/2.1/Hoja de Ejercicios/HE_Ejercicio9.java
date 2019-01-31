f5346c4132fbc0a1dff785d02ffd89b67519e5e8
Jorge López Natal <lopeznatalj@gmail.com>
2019-01-31 12:27:00 +0100

    Add files via upload

--- Tema 2/2.1/Hoja de Ejercicios/HE_Ejercicio9.java
+++ Tema 2/2.1/Hoja de Ejercicios/HE_Ejercicio9.java
@@ -4,9 +4,55 @@ import static es.urjc.etsii.code.concurrency.SimpleConcurrent.*;
 
 public class HE_Ejercicio9 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final int N_FRAGMENTOS = 10;
+	private static final int N_HILOS = 3;
+	
+	private static volatile int[] fichero;
+	private static volatile int fragmentoADescargar;
 
+	private static int descargaDatos(int numFragmento) {
+		sleepRandom(1000);
+		return numFragmento * 2;
+	}
+
+	private static void mostrarFichero() {
+		println("--------------------------------------------------");
+		print("File = [");
+		for (int i = 0; i < N_FRAGMENTOS; i++) {
+			print(fichero[i] + ",");
+		}
+		println("]");
+	}
+
+	public static void downloader(int id) {
+		while (true) {
+			enterMutex();
+			int idxFragmento = fragmentoADescargar++;
+			exitMutex();
+			
+			if (idxFragmento >= N_FRAGMENTOS) {
+				break;
+			}
+			System.out.format("El hilo %d va a descargar el fragmento %d\n", id, idxFragmento);
+			
+			fichero[idxFragmento] = descargaDatos(idxFragmento);
+			System.out.format("El hilo %d ha descargado el fragmento %d\n", id, idxFragmento);
+			if (idxFragmento == (N_FRAGMENTOS - 1)) {
+				System.out.flush();		// Para que otros hilos no impriman sobre el contenido fichero
+				System.out.format("El hilo %d ha descargado el ultimo fragmento\n", id);
+				mostrarFichero();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		fichero = new int[N_FRAGMENTOS];
+		fragmentoADescargar = 0;
+		
+		for (int i = 0; i < N_HILOS; i++) {
+			createThread("downloader", i);
+		}
+		startThreadsAndWait();
 	}
 
 }
